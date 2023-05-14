b63396c01a2ddaafb9b74a0f9293b8b170da979d
TeksMarques <taotaodoce@gmail.com>
2023-05-13 23:53:30 -0300

    feat: criação das rotas

--- shopper-test-front/app/backend/src/routes/managerProduct.routes.ts
+++ shopper-test-front/app/backend/src/routes/managerProduct.routes.ts
@@ -1,1 +1,15 @@
 import { Router } from 'express';
+
+import { ManagerProductController } from '../controllers/managerProduct.controller';
+
+const router = Router();
+
+router.get('/products', ManagerProductController.getAllProducts);
+
+router.get('/products/:code', ManagerProductController.getProductByCode);
+
+router.post('/products', ManagerProductController.createProduct);
+
+router.put('/products/:code', ManagerProductController.updateProduct);
+
+export default router;
